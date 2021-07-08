9fde370de7828aa6b1dd0b56761ce7843095171f
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-08 13:27:14 -0500

    wip

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+require('dotenv').config()
 console.log("index connected")
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+// on Heroku machine, an env variable is called "NODE_ENV" -> "production"
+if (process.env.NODE_ENV === "development") {
+    const cors = require('cors')
+    server.use(cors())
+}
