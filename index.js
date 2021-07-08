7cab99c2d922619d24d271db360c530c9b3899dc
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-08 13:30:19 -0500

    wip

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-console.log("index connected")
 const express = require('express')
 
 const server = express()
@@ -11,3 +10,9 @@ if (process.env.NODE_ENV === "development") {
     const cors = require('cors')
     server.use(cors())
 }
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
