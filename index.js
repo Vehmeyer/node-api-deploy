281713084e54ea88075a3c1c912af7d2d3e59eca
Vehmeyer <j.vehmeyer@gmail.com>
2021-07-08 14:10:06 -0500

    wip

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 // on Heroku machine, an env variable is called "NODE_ENV" -> "production"
 if (process.env.NODE_ENV === 'development') {
@@ -11,8 +13,14 @@ if (process.env.NODE_ENV === 'development') {
     server.use(cors())
 }
 
+// our API comes earlier in the pipeline
+server.get('/api/hello', (req,res) => {
+    res.json({message: 'hello'})
+})
+
+// catch-all that just sends back index.html
 server.use('*', (req, res) => {
-    res.send('<h1>success</h1>')
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 4000
